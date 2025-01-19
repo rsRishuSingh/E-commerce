7d79b977f2f45e75c7d711dd267cdc9b658115ec
Rishu <rk2787824@gmail.com>
2025-01-19 14:48:32 +0530

    Before deployment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react'
 import Item from './Item'
 import { useDispatch } from 'react-redux'
-// import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux'
 import { actionCreators } from '../state/index'
 export default function Home() {
     const dispatch = useDispatch()
-    const [count, setCount] = useState(1)
+    const [count, setCount] = useState(0)
+    const actions = bindActionCreators(actionCreators, dispatch)
 
+
+    // const handleAddCart = () => {
+    //     setCount(count + 1)
+    //     dispatch(actionCreators.withdrawMoney(1100))
+    // }
+    // const handleRemoveCart = () => {
+    //     setCount(count - 1)
+    //     dispatch(actionCreators.depositMoney(1100))
+
+    // }
     const handleAddCart = () => {
         setCount(count + 1)
-        dispatch(actionCreators.withdrawMoney(1100))
+        actions.withdrawMoney(1100)
     }
     const handleRemoveCart = () => {
         setCount(count - 1)
-        dispatch(actionCreators.depositMoney(1100))
+        actions.depositMoney(1100)
 
     }
 
